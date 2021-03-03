22fc4b10f3df09493139362565575dce7cdc00ef
marco-amorim <marcoamorim95@hotmail.com>
2021-03-03 00:49:49 -0300

    removed comments

--- backend/src/main/java/com/softplan/people/manager/controller/AuthController.java
+++ backend/src/main/java/com/softplan/people/manager/controller/AuthController.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//Controller
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class AuthController {
 
     @GetMapping(path = "/auth")
     public AuthenticationBean authenticate() {
-        //throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
         return new AuthenticationBean("You are authenticated");
     }
 }
