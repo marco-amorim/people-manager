8158d4528aabd9bf1917e5c388ee79b06e15b32c
marco-amorim <marcoamorim95@hotmail.com>
2021-03-04 20:07:26 -0300

    adding tests to person creation

--- backend/src/test/java/com/softplan/people/manager/controller/PersonControllerTest.java
+++ backend/src/test/java/com/softplan/people/manager/controller/PersonControllerTest.java
@@ -1,5 +1,8 @@
 package com.softplan.people.manager.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.softplan.people.manager.exception.ResourceNotFoundException;
 import com.softplan.people.manager.model.Person;
 import com.softplan.people.manager.types.Gender;
@@ -9,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.Base64Utils;
 
@@ -28,7 +33,7 @@ public class PersonControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
+    
     // Authenticated Requests
 
     @Test
@@ -106,6 +111,34 @@ public class PersonControllerTest {
                 "Basic " + Base64Utils.encodeToString("admin:admin".getBytes()));
 
         MvcResult result = mvc.perform(request).andReturn();
+
+        assertEquals(400, result.getResponse().getStatus());
+    }
+
+    // createPerson
+
+    @Test
+    public void should_return_bad_request_on_person_missing_required_fields() throws Exception {
+        // missing fields: CPF, Birth Date and Name
+        Person mockPerson = new Person();
+        mockPerson.setNativeFrom("Florianopolis");
+        mockPerson.setNationality("Brazilian");
+        mockPerson.setGender(Gender.MALE);
+        mockPerson.setEmail("marcoamorim95@hotmail.com");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(mockPerson);
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/v1/people").header(HttpHeaders.AUTHORIZATION,
+                "Basic " + Base64Utils.encodeToString("admin:admin".getBytes()));
+
+        MvcResult result = mvc.perform(((MockHttpServletRequestBuilder) request)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+                .characterEncoding("utf-8"))
+                .andReturn();
 
         assertEquals(400, result.getResponse().getStatus());
     }
